b9d39864cf8d217833c491f1ced23657860e8649
sravan kumar <mekalasravankumar@gmail.com>
2024-06-19 14:57:28 +0000

    out is not getting

---
+++ src/components/Speedometer/index.js
@@ -0,0 +1,16 @@
+// Write your code here
+import Components from 'react'
+
+import './index.css'
+
+class Speedometer extends Components {
+  render() {
+    return (
+      <div>
+        <h1 className="heading">Speedometer</h1>
+      </div>
+    )
+  }
+}
+
+export default Speedometer
